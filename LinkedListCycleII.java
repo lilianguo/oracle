2b6b63707eedd7f78c28ce6d4e1f237c32995d20
linqig <linqig@amazon.com>
2019-09-09 18:57:00 -0700

    Create LinkedListCycleII.java

---
+++ LinkedListCycleII.java
@@ -0,0 +1,26 @@
+class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode start = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == slow) {
+            while(start != fast) {
+                start = start.next;
+                fast = fast.next;
+            }
+            return start;
+        } else {
+            return null;
+        }
+    }
+}
