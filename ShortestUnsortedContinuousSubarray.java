82673fc575861c6f49fc372b57b8caca7dee50ee
linqig <linqig@amazon.com>
2019-09-11 17:28:19 -0700

    Create ShortestUnsortedContinuousSubarray.java

---
+++ ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,30 @@
+/*
+Given an integer array, you need to find one continuous subarray that 
+if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
+
+You need to find the shortest such subarray and output its length.
+
+Example 1:
+Input: [2, 6, 4, 8, 10, 9, 15]
+Output: 5
+Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+*/
+
+// [2,1] 
+
+class ShortestUnsortedContinuousSubarray {
+    // 581. Shortest Unsorted Continuous Subarray
+    // nlogn using sort
+    public int findUnsortedSubarrayII(int[] nums) {
+        int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length, end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+    }
+}
